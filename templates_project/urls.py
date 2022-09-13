36a3d2f9d1e3a879c71f118c3b0b3c44785932c7
masamune11x <masamune11x@gmail.com>
2022-09-13 14:29:04 +0900

    first commit

--- templates_project/urls.py
+++ templates_project/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import sign_up_Class, sign_in_Class
 from django.conf import settings
 from django.conf.urls.static import static
+
+from .views import sign_up_Class, sign_in_Class, errorview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sign_up/', sign_up_Class.as_view()),
     path('sign_in/', sign_in_Class.as_view()),
+    path('error/', errorview),
 ]+ static (settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- templates_project/views.py
+++ templates_project/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 class sign_up_Class(TemplateView):
     template_name = 'sign_up.html'
 
 class sign_in_Class(TemplateView):
     template_name = 'sign_in.html'
+
+    
+
+def errorview(request):
+    errormesage = "パスワードが正しくありません"
+    return render(request, 'error.html', {'errormessage':errormesage})
