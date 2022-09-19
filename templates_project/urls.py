58afacad9c7c5d301edcc2ca6f1ac079972f31ac
Shiki Yoshida <shikiyoshida@Alexanders-MacBook-Air.local>
2022-09-19 10:53:38 +0900

    urlを変更

--- templates_project/urls.py
+++ templates_project/urls.py
@@ -8,7 +8,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('photo_share_app.urls')),
-    path('photo_share_app/', include('photo_share_app.urls')),
+    
 ]\
 + static (settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
 + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
